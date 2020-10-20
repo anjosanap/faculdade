6acfd0939373c9e82029dc46fac54edbb62f5ed3
Ana Paula Anjos <anjosanapaula@outlook.com>
2020-10-19 21:50:02 -0300

    aula 19-10-2020

---
+++ 1_semestre/logica_programacao/aulas/timer.py
@@ -0,0 +1,9 @@
+# Altere o programa anterior de modo que os
+# segundos sejam exibidos no formato de um
+# relógio digital, ou sejam hh:mm:ss.
+
+s = 0
+
+while s < 60:
+    print('00:00:%02d' % s)
+    s += 1
---
+++ 1_semestre/logica_programacao/aulas/timer_seconds.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+while True:
+    h = 0
+    while h < 24:
+        m = 0
+        while m < 60:
+            s = 0
+            while s < 60:
+                print('%02d:%02d:%02d' % (h, m, s))
+                s += 1
+                sleep(1)
+            m += 1
+        h += 1
+
+# while True:
+#     for h in range(24):
+#         for m in range(60):
+#             for s in range(60):
+#                 print('%02d:%02d:%02d' % (h, m, s))
+#                 sleep(1)
